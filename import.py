10066b37b231f8fb9185559258785cf59f4504b7
DineshKumar9412 <83091156+DineshKumar9412@users.noreply.github.com>
2022-09-22 19:58:38 +0530

    Update import.py

--- import.py
+++ import.py
@@ -21,3 +21,53 @@ Python Json
 
 https://medium.com/@alains/python-tutorial-7-advanced-features-of-the-json-library-4b5c485c75bf
 
+Dlib_Model
+******************
+
+
+https://programmer.help/blogs/face-recognition-dlib-python-version.html
+
+https://towardsdatascience.com/step-by-step-face-recognition-code-implementation-from-scratch-in-python-cc95fa041120
+
+https://github.com/susantabiswas/FaceRecog
+
+https://www.geeksforgeeks.org/python-multiple-face-recognition-using-dlib/
+
+Face Detector HOG & CNN
+*****************************
+
+https://learnopencv.com/face-detection-opencv-dlib-and-deep-learning-c-python/
+
+https://towardsdatascience.com/cnn-based-face-detector-from-dlib-c3696195e01c
+
+https://livecodestream.dev/post/detecting-face-features-with-python/
+  
+  
+Folder Looping
+*****************
+
+for directory in os.listdir('images'):
+    for file in os.listdir(os.path.join('images', directory)):
+        img_path = os.path.join('images', directory, file)
+        img = cv2.imread(img_path)
+        
+        
+GOOGLE COLAB 
+*****************
+
+How to unzip file in colab 
+
+1.  !unzip -q "/content/gdrive/My Drive/dataset.zip"
+
+  2.  !tar -xf lfw.tgz
+
+How to Delete folder or file in colab 
+
+https://www.codegrepper.com/code-examples/python/delete+non+empty+directory+python
+
+1. import os
+2. import shutil
+3. os.remove('/your/path/to/file.txt') #removes a file.
+4. os.rmdir('/your/folder/path/') #removes an empty directory.
+5. shutil.rmtree('/your/folder/path/') #deletes a directory and all its contents.
+
