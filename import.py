cbafbb9294580d79bdabd6fad1674616dcd838b6
DineshKumar9412 <83091156+DineshKumar9412@users.noreply.github.com>
2022-09-22 19:57:07 +0530

    Create import.py

---
+++ import.py
@@ -0,0 +1,23 @@
+Object Shape 
+**************
+
+https://www.google.com/search?q=opencv+shape+detection+python&ei=epgCYvflJ4G_8QOc6IKIBg&oq=opencv+shape+python&gs_lcp=Cgdnd3Mtd2l6EAMYADIGCAAQBxAeMgYIABAHEB4yBQgAEIAEMggIABAIEAcQHjIICAAQCBAHEB4yCAgAEAgQBxAeMgYIABAIEB4yBggAEAgQHjIGCAAQCBAeMgYIABAIEB46BwgAEEcQsAM6BwgAELADEEM6BwgAELEDEEM6BAgAEEM6BAgAEA1KBAhBGABKBAhGGABQ5wNY-RFg7ypoAXACeACAAWqIAbYEkgEDNS4xmAEAoAEByAEKwAEB&sclient=gws-wiz
+
+
+
+Killer Python Automation Scripts for Daily Problems
+********************************************************
+
+https://python.plainenglish.io/10-python-automation-scripts-for-your-daily-problems-aefb502969e2
+
+https://python.plainenglish.io/9-killer-python-automation-scripts-for-daily-problems-c1715ed42ed5
+
+
+Python BEST website 
+
+https://data-flair.training/blogs/top-python-interview-questions-answer
+
+Python Json
+
+https://medium.com/@alains/python-tutorial-7-advanced-features-of-the-json-library-4b5c485c75bf
+
